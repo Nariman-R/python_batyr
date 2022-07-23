2b833f6fc23cafc67c970e96675b227546cac76d
Nariman <87778834+Nariman-R@users.noreply.github.com>
2022-07-23 13:32:16 +0600

    add migrations 1
    add new field is_issued in payment table

---
+++ app/migrations/1_add_field_to_payment.py
@@ -0,0 +1,17 @@
+from playhouse.migrate import *
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from dependencies import get_db
+
+
+db = get_db()
+migrator = PostgresqlMigrator(db)
+
+if __name__ == "__main__":
+    migrate(
+        migrator.add_column('payment', 'is_issued', BooleanField(verbose_name="Есть ошибка или нет", default=False)),
+    )
