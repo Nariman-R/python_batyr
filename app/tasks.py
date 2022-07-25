188dc4455e2bfa891544a1afb66e124e8e9f7185
Nariman <87778834+Nariman-R@users.noreply.github.com>
2022-07-25 20:17:49 +0600

    add tasks.py

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,6 @@
 def fill_file_with_hw():
     with open("file.txt", 'w') as file:
-        for i in range(1, 10001):
+        for i in range(1, 1001):
             file.write("Hello World\n")
 
 if __name__ == '__main__':
