c216e2f1cc669bba75ef588cf44987ae76a5783d
Nariman <87778834+Nariman-R@users.noreply.github.com>
2022-07-29 18:50:54 +0600

    refactor scheduler

--- app/scheduler.py
+++ app/scheduler.py
@@ -1,18 +1,17 @@
-
 from datetime import datetime
 from rq_scheduler import Scheduler
 
 from dependencies import get_redis
-from tasks import check_payment
+from tasks import check_payments
 
 RUN_INTERVAL = 30
+
 
 def create_scheduler():
     redis = get_redis()
     scheduler = Scheduler(connection=redis)
     scheduler.schedule(
         scheduled_time=datetime.utcnow(),
-        func=check_payment,
+        func=check_payments,
         interval=RUN_INTERVAL,
-
-)
+    )
