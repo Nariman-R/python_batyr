d8b612f6b77bb3f639d5560fcf7093fac1ff3e27
Nariman <87778834+Nariman-R@users.noreply.github.com>
2022-07-29 16:48:05 +0600

    add issue_paid_item

--- app/tasks.py
+++ app/tasks.py
@@ -4,22 +4,29 @@ from models import Payment
 
 PURCHASE_TIME_LIMIT = 60
 
+
 def fill_file_with_hw():
-    with open("file.txt", 'w') as file:
-        for i in range(1, 1001):
+    with open("file.txt", "w") as file:
+        for line in range(1, 1001):
             file.write("Hello World\n")
 
 
 def check_payment():
-  new_payment_list = Payment.select().where(Payment.status == 'new')
+    new_payment_list = Payment.select().where(Payment.status == "new")
 
-  for payment in new_payment_list:
-    payment_lifetime = payment.date + timedelta(seconds=PURCHASE_TIME_LIMIT)
-    print(payment_lifetime, payment.status)
-    if payment_lifetime < datetime.now():
-      payment.status = 'not paid'
-      payment.save()
+    for payment in new_payment_list:
+        payment_lifetime = payment.date + timedelta(seconds=PURCHASE_TIME_LIMIT)
+        print(payment_lifetime, payment.status)
+        if payment_lifetime < datetime.now():
+            payment.status = "not_paid"
+            payment.save()
 
 
-if __name__ == '__main__':
+def issue_paid_item(payment_id):
+    paid_payment = Payment.get_by_id(payment_id)
+    paid_payment.is_issued = True
+    paid_payment.save()
+
+
+if __name__ == "__main__":
     fill_file_with_hw()
