06eaef0855fff2d2203d297ff191e325f0b5493e
Nariman <87778834+Nariman-R@users.noreply.github.com>
2022-07-29 11:15:47 +0600

    delete file fill_file_with_hw.py
    move function fill_file_with_hw.py to tasks.py

--- app/fill_file_with_hw.py
+++
@@ -1,7 +0,0 @@
-def fill_file_with_hw():
-    with open("file.txt", 'w') as file:
-        for i in range(1, 10001):
-            file.write("Hello World\n")
-
-if __name__ == '__main__':
-    fill_file_with_hw()
